28b4c49ac13e4250dcf062949906b5fcf8927b40
porterwilcox <wilcox.porter@gmail.com>
2019-03-28 19:09:08 -0600

    init

---
+++ client/src/store.js
@@ -0,0 +1,94 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Axios from 'axios'
+import router from './router'
+
+Vue.use(Vuex)
+
+//Allows axios to work locally or live
+let base = window.location.host.includes('localhost:8080') ? '//localhost:3000' : '/'
+
+let auth = Axios.create({
+  baseURL: base + "auth/",
+  timeout: 3000,
+  withCredentials: true
+})
+
+let api = Axios.create({
+  baseURL: base + "api/",
+  timeout: 3000,
+  withCredentials: true
+})
+
+export default new Vuex.Store({
+  state: {
+    user: {},
+    boards: [],
+    activeBoard: {}
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user
+    },
+    setBoards(state, boards) {
+      state.boards = boards
+    }
+  },
+  actions: {
+    //#region -- AUTH STUFF --
+    register({ commit, dispatch }, newUser) {
+      auth.post('register', newUser)
+        .then(res => {
+          commit('setUser', res.data)
+          router.push({ name: 'boards' })
+        })
+    },
+    authenticate({ commit, dispatch }) {
+      auth.get('authenticate')
+        .then(res => {
+          commit('setUser', res.data)
+          router.push({ name: 'boards' })
+        })
+        .catch(res => {
+          router.push({ name: 'login' })
+        })
+    },
+    login({ commit, dispatch }, creds) {
+      auth.post('login', creds)
+        .then(res => {
+          commit('setUser', res.data)
+          router.push({ name: 'boards' })
+        })
+    },
+    //#endregion
+
+
+    //#region -- BOARDS --
+    getBoards({ commit, dispatch }) {
+      api.get('boards')
+        .then(res => {
+          commit('setBoards', res.data)
+        })
+    },
+    addBoard({ commit, dispatch }, boardData) {
+      api.post('boards', boardData)
+        .then(serverBoard => {
+          dispatch('getBoards')
+        })
+    },
+    deleteBoard({ commit, dispatch }, boardId) {
+      api.delete('boards/' + boardId)
+        .then(res => {
+          dispatch('getBoards')
+        })
+    }
+    //#endregion
+
+
+    //#region -- LISTS --
+
+
+
+    //#endregion
+  }
+})
